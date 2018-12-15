a7be8c89ce6a9f82d22ec22de5a8f35d77c3e57e
Aleksandr Vinogradov <vsasha1305@gmail.com>
2018-12-15 20:13:43 +0300

    before new work

--- Akinator/AkinatorPro/AkinatorPro/main.cpp
+++ Akinator/AkinatorPro/AkinatorPro/main.cpp
@@ -7,23 +7,30 @@
 #include <assert.h>
 #include <string.h>
 
-#include "../../../../Libs/FileReaderLib/ReaderLib.h"
-#include "../../../../Libs/TreeLib/tree.h"
+#include "FileReaderLib/ReaderLib.h"
+#include "TreeLib/tree.h"
 
 #define FILE_WITH_TREE "../../data/tree.txt"
 #define FILE_WITH_STRINGS "../../data/strings.txt"
 
-#define FILE_DOT "../../data/dot.dot"
+#define FILE_DOT "../../data/dot1.dot"
 #define PNG_FILE "../../data/png.png"
 
-#define BUFF_STRING_SIZE 100
+constexpr auto BUFF_STRING_SIZE = 100;
 
 void StartSearch(Node * tree);
 void Sorry();
 char * EnterNewName();
 bool AskQuestion(char * string);
 void Grats();
-char * GetQuestion(Node * node , char * new_name);
+char * GetQuestion(Node * node, char * new_name);
+
+Node * TreeSearch(Node * tree, const char * string);
+
+void DescribeObject(Node * tree);
+
+void DifferenceObject(Node * start1, Node * start2);
+
 
 int main()
 {
@@ -32,16 +39,19 @@ int main()
     size_t size_of_file = ReadFromFile(FILE_WITH_STRINGS, &buff);
     char ** strings = nullptr;
     size_t amount_of_strings = ParseStringToStrings(buff, &strings);
-    free(buff);
 
     //reading tree
     Node * tree = nullptr;
     size_t amount_of_nodes = ReadTreeFromFileWithStringsNumbers(&tree, strings, amount_of_strings, FILE_WITH_TREE);
 
-    TreeDumpDot(tree, FILE_DOT, PNG_FILE);
-
     while (true) {
-        puts("\"find\" to find name of thing, \"stop\" to exit");
+        puts(" \
+            \"find\" to find name of thing\n \
+            \"show_tree\" to open tab with tree\n \
+            \"save_tree\" to save tree picture in file\n \
+            \"describe\" to get describtion of object\n \
+            \"difference\" to see difference of two objects\n \
+            \"stop\" to exit\n");
         char mode[BUFF_STRING_SIZE] = "";
         gets_s(mode, BUFF_STRING_SIZE);
         if (strcmp(mode, "find") == 0) {
@@ -53,12 +63,45 @@ int main()
         {
             break;
         }
+        else if (strcmp(mode, "show_tree") == 0) {
+            TreeDumpDot(tree, FILE_DOT, PNG_FILE, 1);
+        }
+        else if (strcmp(mode, "save_tree") == 0) {
+            TreeDumpDot(tree, FILE_DOT, PNG_FILE);
+        }
+        else if (strcmp(mode, "describe") == 0) {
+            puts("Who you want to describe ?");
+            gets_s(mode, BUFF_STRING_SIZE);
+            Node * start = TreeSearch(tree, mode);
+            if (start != nullptr) {
+                DescribeObject(start);
+            }
+            else {
+                puts("can't find");
+            }
+        }
+        else if (strcmp(mode, "difference") == 0) {
+            puts("between who?");
+            gets_s(mode, BUFF_STRING_SIZE);
+            Node * start1 = TreeSearch(tree, mode);
+            puts("and who?");
+            gets_s(mode, BUFF_STRING_SIZE);
+            Node * start2 = TreeSearch(tree, mode);
+            if (start1 != nullptr && start2 != nullptr) {
+                DifferenceObject(start1, start2);
+            }
+            else {
+                puts("can't find");
+            }
+        }
         else {
             puts(" can't resolve command");
         }
     }
 
-    FreeStrings(strings, amount_of_strings);
+    SaveTreeToFile(tree, FILE_WITH_TREE, FILE_WITH_STRINGS);
+
+    free(buff);
     NodeFreeRecur(tree);
 
     return 0;
@@ -128,6 +171,120 @@ char * GetQuestion(Node * node, char * new_name)
     char * buff = (char*)calloc(BUFF_STRING_SIZE, sizeof(char));
     gets_s(buff, BUFF_STRING_SIZE);
     return buff;
+}
+
+Node * TreeSearch(Node * tree, const char * string)
+{
+    assert(tree != nullptr);
+    if (strcmp(string, tree->data) == 0) {
+        return tree;
+    }
+    if (tree->left != nullptr) {
+        Node * result = TreeSearch(tree->left, string);
+        if (result != nullptr) {
+            return result;
+        }
+    }
+    if (tree->right != nullptr) {
+        Node * result = TreeSearch(tree->right, string);
+        if (result != nullptr) {
+            return result;
+        }
+    }
+    return nullptr;
+}
+
+void DescribeObject(Node * tree)
+{
+    assert(tree != nullptr);
+
+    if (tree->parent != nullptr) {
+        printf("this object is is %s\n", tree->data);
+        DescribeObject(tree->parent);
+    }
+}
+
+void DifferenceObject(Node * start1, Node * start2)
+{
+    assert(start1 != nullptr);
+    assert(start2 != nullptr);
+
+    if (start1->parent != nullptr) {
+        if (start2->parent != nullptr) {
+            Node * copy_1 = start1->parent;
+            Node * copy_2 = start2->parent;
+            while ((copy_1->parent != copy_2->parent) && copy_1->parent != nullptr && copy_2->parent != nullptr)
+            {
+                copy_1 = copy_1->parent;
+            }
+            if (copy_1->parent == copy_2->parent) {
+                printf("%s is %s then %s\n", start1->data, copy_1->data, start2->data);
+                while (copy_1 != nullptr) {
+                    printf("%s and %s same in %s\n", start1->data, start2->data, copy_1->data);
+                    copy_1 = copy_1->parent;
+                }
+                return;
+            }
+            else {
+                while ((copy_1->parent != copy_2->parent) && copy_1->parent != nullptr && copy_2->parent != nullptr)
+                {
+                    copy_2 = copy_2->parent;
+                }
+                if (copy_1->parent == copy_2->parent) {
+                    printf("%s is %s then %s\n", start1->data, copy_1->data, start2->data);
+                    while (copy_1 != nullptr) {
+                        printf("%s and %s same in %s\n", start1->data, start2->data, copy_1->data);
+                        copy_1 = copy_1->parent;
+                    }
+                    return;
+                }
+            }
+        }
+        else {
+            puts("Error, is object is defference");
+        }
+    }
+    else {
+        puts("Error, is object is defference");
+    }
+    if (start1->parent != nullptr) {
+        if (start2->parent != nullptr) {
+            Node * copy_1 = start1->parent;
+            Node * copy_2 = start2->parent;
+            while ((copy_1->parent != copy_2->parent) && copy_1->parent != nullptr && copy_2->parent != nullptr)
+            {
+                copy_2 = copy_2->parent;
+            }
+            if (copy_1->parent == copy_2->parent) {
+                printf("%s is %s then %s\n", start1->data, copy_2->data, start2->data);
+                while (copy_2 != nullptr) {
+                    printf("%s and %s same in %s\n", start1->data, start2->data, copy_2->data);
+                    copy_1 = copy_2->parent;
+                }
+                return;
+            }
+            else{
+                while ((copy_1->parent != copy_2->parent) && copy_1->parent != nullptr && copy_2->parent != nullptr)
+                {
+                    copy_1 = copy_1->parent;
+                }
+                if (copy_1->parent == copy_2->parent) {
+                    printf("%s is %s then %s\n", start1->data, copy_2->data, start2->data);
+                    while (copy_2 != nullptr) {
+                        printf("%s and %s same in %s\n", start1->data, start2->data, copy_2->data);
+                        copy_1 = copy_2->parent;
+                    }
+                    return;
+                }
+            }
+        }
+        else {
+            puts("Error, is object is defference");
+        }
+    }
+    else {
+        puts("Error, is object is defference");
+    }
 }
 
 /// save tree !!!!
--- Hash/Hash/main.cpp
+++ Hash/Hash/main.cpp
@@ -30,9 +30,15 @@
 
 #define FILE_OUT_6 "../data/hash6.csv"
 
+#define AMOUNT_OF_ROWS 500
 
+#define MAX_STRING_SIZE 500
 
-#define AMOUNT_OF_ROWS 25
+struct HashTable {
+    int(*HashFunc)(char * string);
+    List * hash_table[AMOUNT_OF_ROWS];
+};
+
 
 int GetHash_v1(char * strings);
 int GetHash_v2(char * strings);
@@ -41,11 +47,23 @@ int GetHash_v4(char * strings);
 int GetHash_v5(char * strings);
 int GetHash_v6(char * strings);
 
-void Test(char ** strings, size_t amount_of_strings, int hash_func(char * string), size_t lists_amount, List ** lists);
+void Test(char ** strings, size_t amount_of_strings, int hash_func(char * string), size_t lists_amount, HashTable * hashTable);
 
-void SaveResults(const char * file_output, List ** list, size_t lists_amount);
+void SaveResults(const char * file_output, HashTable * hashTable, size_t lists_amount);
 
 void TestMethod(char ** strings, size_t amount_of_strings, int hash_func(char * string), const char * file_output);
+
+void HashTableInsert(char * string, HashTable * hashtable);
+
+int ConvertToIndex(int hashcode);
+
+char * HashTableFind(char * string, HashTable * hashtable);
+
+size_t ParseStringToStringsCustom(char * string_to_parse, char *** strings);
+
+struct HashTable* HashTableCtor(int(*HashFunc)(char * string));
+
+void HashTableDtor(HashTable * hashTable);
 
 
 int main()
@@ -53,11 +71,10 @@ int main()
     char * buff;
     size_t size_of_buff = ReadFromFile(FILE_INPUT, &buff);
 
-    char ** strings = nullptr;
-    size_t amount_of_strings = ParseStringToStrings(buff, &strings);
+    char ** strings;
+    size_t amount_of_strings = ParseStringToStringsCustom(buff, &strings);
 
-    //free buff
-    free(buff);
+    //PrintStrings(strings, amount_of_strings);
 
     TestMethod(strings, amount_of_strings, GetHash_v1, FILE_OUT_1);
     TestMethod(strings, amount_of_strings, GetHash_v2, FILE_OUT_2);
@@ -65,8 +82,8 @@ int main()
     TestMethod(strings, amount_of_strings, GetHash_v4, FILE_OUT_4);
     TestMethod(strings, amount_of_strings, GetHash_v5, FILE_OUT_5);
 
-    //free strings
-    StringsMemClear(strings, amount_of_strings);
+    free(buff);
+    free(strings);
 
     return 0;
 }
@@ -87,7 +104,7 @@ int GetHash_v3(char * strings)
     for (size_t i = 0; i < lenght; i++) {
         bit_sum += strings[i];
     }
-    return bit_sum;
+    return (bit_sum >= 0) ? bit_sum : -bit_sum;
 }
 
 int GetHash_v4(char * strings)
@@ -95,9 +112,9 @@ int GetHash_v4(char * strings)
     size_t lenght = strlen(strings);
     int bit_sum = 0;
     for (size_t i = 0; i < lenght; i++) {
-        bit_sum += (strings[i] ^ strings[i] & 0x0F);
-    }
-    return bit_sum;
+        bit_sum += (strings[i] & 0x0F);
+    } 
+    return (bit_sum >= 0) ? bit_sum : -bit_sum;
 }
 
 int GetHash_v5(char * strings)
@@ -105,9 +122,9 @@ int GetHash_v5(char * strings)
     size_t lenght = strlen(strings);
     int bit_sum = 0;
     for (size_t i = 0; i < lenght; i++) {
-        bit_sum += (((strings[i] ^ strings[i] & 0x0F) << 4) | ((strings[i] ^ strings[i] & 0xF0) >> 4));
+        bit_sum += (((~strings[i] & 0x0F) << 4) | ((~strings[i] & 0xF0) >> 4));
     }
-    return bit_sum;
+    return (bit_sum >= 0) ? bit_sum : -bit_sum;
 }
 int GetHash_v6(char * strings)
 {
@@ -115,39 +132,98 @@ int GetHash_v6(char * strings)
     return 0;
 }
 
-void Test(char ** strings, size_t amount_of_strings, int hash_func(char * string), size_t lists_amount, List ** list)
+void Test(char ** strings, size_t amount_of_strings, int hash_func(char * string), size_t lists_amount, HashTable * hashTable)
 {
     for (size_t i = 0; i < amount_of_strings; i++) {
-        int hash = hash_func(strings[i]) % lists_amount;
-        ListInsert(list[hash], strings[i]);
+        HashTableInsert(strings[i], hashTable);
     }
 }
 
 
-void SaveResults(const char * file_output, List ** list, size_t lists_amount)
+void SaveResults(const char * file_output, HashTable * hashTable, size_t lists_amount)
 {
     OpenFile(file_output, "w");
     fputs("hash value,amount of strings\n", file);
     for (size_t i = 0; i < lists_amount; i++) {
-        fprintf_s(file, "%d,%d\n", i, list[i]->size);
+        fprintf_s(file, "%d,%d\n", i, hashTable->hash_table[i]->size);
     }
     fclose(file);
 }
 
 void TestMethod(char ** strings, size_t amount_of_strings, int hash_func(char *string), const char * file_output)
 {
-    List  * lists[AMOUNT_OF_ROWS];
+    HashTable * hashTable = HashTableCtor(hash_func);
 
-    for (size_t i = 0; i < AMOUNT_OF_ROWS; i++) {
-        lists[i] = ListCtorDin();
-    }
     //performing test 
-    Test(strings, amount_of_strings, hash_func, AMOUNT_OF_ROWS, lists);
+    Test(strings, amount_of_strings, hash_func, AMOUNT_OF_ROWS, hashTable);
 
     //save results
-    SaveResults(file_output, lists, AMOUNT_OF_ROWS);
-    //free lists
-    for (size_t i = 0; i < AMOUNT_OF_ROWS; i++) {
-        ListDtorDin(lists[i]);
+    SaveResults(file_output, hashTable, AMOUNT_OF_ROWS);
+
+    HashTableDtor(hashTable);
+}
+
+void HashTableInsert(char * string, HashTable * hashtable)
+{
+    assert(string != nullptr);
+    assert(hashtable != nullptr);
+    int hash = hashtable->HashFunc(string);
+    int index = ConvertToIndex(hash);
+    ListInsert(hashtable->hash_table[index], string);
+}
+
+int ConvertToIndex(int hashcode)
+{
+    return hashcode % AMOUNT_OF_ROWS;
+}
+
+char * HashTableFind(char * string, HashTable * hashtable)
+{
+    assert(string != nullptr);
+    assert(hashtable != nullptr);
+    int hash = hashtable->HashFunc(string);
+    int index = ConvertToIndex(hash);
+    return (ListSearch(hashtable->hash_table[index], string)->data);
+}
+
+size_t ParseStringToStringsCustom(char * string_to_parse, char *** strings)
+{
+    size_t strings_amount = CountStrings(string_to_parse);
+    *strings = (char**)calloc(strings_amount, sizeof(char*));
+    char * buff_copy = string_to_parse;
+    size_t current_string = 0;
+    (*strings)[current_string] = buff_copy;
+    current_string++;
+    while (*buff_copy != '\0') {
+        if (*buff_copy == '\n') {
+            *buff_copy = '\0';
+            if (*(buff_copy + 1) != '\0') {
+                (*strings)[current_string] = buff_copy + 1;
+                current_string++;
+            }
+        }
+        if (*(buff_copy) == ' ') {
+            *buff_copy = '\0';
+        }
+        buff_copy++;
     }
+    return current_string;
+}
+
+HashTable* HashTableCtor(int(*HashFunc)(char * string))
+{
+    HashTable * hashTable = (HashTable*)calloc(1, sizeof(HashTable));
+    hashTable->HashFunc = HashFunc;
+    for (size_t i = 0; i < AMOUNT_OF_ROWS; i++) {
+        (*hashTable).hash_table[i] = ListCtorDin();
+    }
+    return hashTable;
+}
+
+void HashTableDtor(HashTable * hashTable)
+{
+    for (size_t i = 0; i < AMOUNT_OF_ROWS; i++) {
+        ListDtorDin(hashTable->hash_table[i]);
+    }
+    free(hashTable);
 }
